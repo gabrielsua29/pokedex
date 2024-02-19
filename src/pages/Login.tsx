775829eab891df91c9297e8736ad2969c2dfd647
gabrielsua29 <ernesriz32@gmail.com>
2024-02-19 16:59:10 +0000

    Pokedex_Ver.1.0.2.2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./login.module.css"
+import { Center, Heading, Flex, Input, Button } from '@chakra-ui/react'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+  } from '@chakra-ui/react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")   
+    const [hasInterected, setHasInterected] = useState(false) 
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value) 
+        setHasInterected(true)
+    }
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)
+
+    const handleFormSubmit = () => {
+        console.log("login")
+    }
+
     
     return(
         <>
-        
+            <Center className={styles.loginContainer} bg='blue' w='100%' p={4} color='white'>
+                <Flex direction="column" align="center">
+                    <Heading className={styles.loginTitle} as="h2" size="3xl" noOfLines={1}>
+                        Login
+                    </Heading>
+                    <form onSubmit={handleFormSubmit}>
+                    <FormControl className={styles.loginFormWrapper}>
+                        <FormLabel>Email</FormLabel>
+                        <Input id="emailInput" type="email" onChange={handleEmailChange} isRequired />
+                        {hasInterected && email === "" &&
+                            <FormErrorMessage>Email is required.</FormErrorMessage>
+                        }
+                        <FormLabel>Password</FormLabel>
+                        <Input id="passInput" type="password" onChange={handlePasswordChange} isRequired />
+                        <Button mt={4} colorScheme="teal" type="submit">
+                            Login
+                        </Button>
+                    </FormControl>
+                    </form>
+                </Flex>    
+            </Center>  
         </>
     )
 }
--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -7,6 +7,10 @@ import { fetchPokemons } from "../api/fetchPokemons";
 import { Pokemon } from "../types/types";
 import LoadingScreen from "../components/LoadingScreen";
 import { waitFor } from "../utils/utils";
+import { 
+    grassIcon, poisonIcon, bugIcon, darkIcon, dragonIcon, electricIcon, fairyIcon, fightingIcon, fireIcon,
+    flyingIcon, ghostIcon, groundIcon, iceIcon, normalIcon, psychicIcon, rockIcon, steelIcon, waterIcon 
+} from "./pokemonTypesIconsExport";
 
 const Pokemons = () => {
     const [isLoading, setIsLoading] = useState(false)
@@ -52,6 +56,63 @@ const Pokemons = () => {
                             <div className={styles.listItemText}>
                                 <span>{pokemon.name}</span>
                                 <span>{pokemon.id}</span>
+                                <div className={styles.iconContainer}>
+                                {pokemon.type.toString().toLowerCase().includes("grass") &&
+                                    <img className={styles.pokemonTypeIcon} src={grassIcon} alt="Grass" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("poison") &&
+                                    <img className={styles.pokemonTypeIcon} src={poisonIcon} alt="Poison" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("bug") &&
+                                    <img className={styles.pokemonTypeIcon} src={bugIcon} alt="Bug" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("dark") &&
+                                    <img className={styles.pokemonTypeIcon} src={darkIcon} alt="Dark" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("dragon") &&
+                                    <img className={styles.pokemonTypeIcon} src={dragonIcon} alt="Dragon" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("electric") &&
+                                    <img className={styles.pokemonTypeIcon} src={electricIcon} alt="Electric" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("fairy") &&
+                                    <img className={styles.pokemonTypeIcon} src={fairyIcon} alt="Fairy" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("fighting") &&
+                                    <img className={styles.pokemonTypeIcon} src={fightingIcon} alt="Fighting" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("fire") &&
+                                    <img className={styles.pokemonTypeIcon} src={fireIcon} alt="Fire" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("flying") &&
+                                    <img className={styles.pokemonTypeIcon} src={flyingIcon} alt="Flying" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("ghost") &&
+                                    <img className={styles.pokemonTypeIcon} src={ghostIcon} alt="Ghost" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("ground") &&
+                                    <img className={styles.pokemonTypeIcon} src={groundIcon} alt="Ground" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("ice") &&
+                                    <img className={styles.pokemonTypeIcon} src={iceIcon} alt="Ice" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("normal") &&
+                                    <img className={styles.pokemonTypeIcon} src={normalIcon} alt="Normal" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("psychic") &&
+                                    <img className={styles.pokemonTypeIcon} src={psychicIcon} alt="Physic" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("rock") &&
+                                    <img className={styles.pokemonTypeIcon} src={rockIcon} alt="Rock" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("steel") &&
+                                    <img className={styles.pokemonTypeIcon} src={steelIcon} alt="Steel" />
+                                }
+                                {pokemon.type.toString().toLowerCase().includes("water") &&
+                                    <img className={styles.pokemonTypeIcon} src={waterIcon} alt="Water" />
+                                }
+                                </div>
+
                             </div>
                         </Link>
                     ))}
